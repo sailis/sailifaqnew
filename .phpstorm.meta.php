26488d9ff1c652666f15adb66028128b9e75c335
sailis <sailiheart1>
2018-12-09 05:36:35 -0500

    Task: 3 Tests, 3 Assertions

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-12-09 05:35:41.
+    * Generated on 2018-12-09 09:42:57.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
